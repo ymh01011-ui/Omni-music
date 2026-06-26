b479f00191cf86bedf2dbba923a610dbba11ed13
ymh01011-ui <ymh01011@gmail.com>
2026-06-26 19:28:39 +0300

    Create OmniMusicDatabase for local Room database

---
+++ app/src/main/java/com/omnimusic/app/data/localdb/OmniMusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.omnimusic.app.data.localdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.omnimusic.app.data.localdb.daos.PlaylistDao
+import com.omnimusic.app.data.localdb.entities.PlaylistEntity
+import com.omnimusic.app.data.localdb.entities.PlaylistSongCrossRef
+import com.omnimusic.app.data.localdb.entities.PlaylistSongEntity
+
+@Database(
+    entities = [
+        PlaylistEntity::class,
+        PlaylistSongEntity::class,
+        PlaylistSongCrossRef::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class OmniMusicDatabase : RoomDatabase() {
+    abstract fun playlistDao(): PlaylistDao
+}
